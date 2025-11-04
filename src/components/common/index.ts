15a6d87a904d2e2ebdcce6031a3d397b6432af0d
centonhuang <centonhuang@tencent.com>
2025-11-05 01:38:06 +0800

    refactor: remove all

--- src/components/common/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Common reusable components
- */
-
-export { LoadingSpinner } from './LoadingSpinner';
-export { ErrorBoundary } from './ErrorBoundary';
-export { EmptyState } from './EmptyState';
-export { DataTable } from './DataTable';
-export { ConfirmDialog } from './ConfirmDialog';
--- src/types/api/common.types.ts
+++
@@ -1,6 +0,0 @@
-export interface PageInfo {
-    total: number;
-    page: number;
-    pageSize: number;
-}
-  
